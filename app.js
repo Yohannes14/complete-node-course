40cef3296c9d339ebf67f8e70477c93f8eec595c
Yohannes <ydegu@admin.crossroads.co>
2023-02-10 11:02:40 +0300

    sharing Data Across Requests

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
+const path = require('path');
 
-const path =require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const bodyParser =require('body-parser');
-
-// parsing incoming requestst
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Filtering paths
-app.use('/admin',adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-// pages not found
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-
-
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,23 @@
 const path = require('path');
+
 const express = require('express');
 
-/// helper function for navigation
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
-// for get 
-router.get('/add-product', (req, res, next)=> {
-    res.sendFile(path.join(rootDir, 'views', 'addProduct.html'));
-});
-//for post
-router.post('/add-product', (req, res, next) =>{
-    console.log(req.body);
-    res.redirect('/');
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-module.exports =router;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
