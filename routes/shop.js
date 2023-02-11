f312d92468b433a372b29ab7b148a0efd2d0790c
Yohannes <ydegu@admin.crossroads.co>
2023-02-11 09:40:18 +0300

    all controller

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productCont =require('../controllers/products')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
-});
+router.get('/',productCont.getProducts );
 
 module.exports = router;
