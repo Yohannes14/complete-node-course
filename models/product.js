3e5a276b47aae90531c6bc8afc7161fd181ec304
Yohannes <ydegu@admin.crossroads.co>
2023-02-11 10:39:27 +0300

    store and fetch data files

--- models/product.js
+++ models/product.js
@@ -1,15 +1,39 @@
-const products =[];
-module.exports =class Product {
-    constructor(t){
-        this.title =t;
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
 
     }
-    save(){
-        products.push(this);
+    save() {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data', 'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-       return products;
+    static fetchAll(cd) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data', 'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+              cd([]);
+            }
+            cd(JSON.parse(fileContent));
+        });
     }
 
 };
