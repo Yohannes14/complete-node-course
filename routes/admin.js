0903d4d218428a01213d67f91cbe544c0e6e868a
Yohannes <ydegu@admin.crossroads.co>
2023-02-11 09:42:34 +0300

    all controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const path = require('path');
+
 const express = require('express');
-const productsCont = require('../controllers/products');
+
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product',productsCont.getAddProduct);
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsCont.postAddProduct);
+router.post('/add-product', productsController.postAddProduct);
 
-module.exports =router;
+module.exports = router;
