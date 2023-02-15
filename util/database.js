cdac579dde0f8272533af1a2a060832d61c68a2c
Yohannes <ydegu@admin.crossroads.co>
2023-02-15 10:24:55 +0300

    connecting db

--- util/database.js
+++ util/database.js
@@ -2,13 +2,28 @@
 const mongodb =require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect('mongodb+srv://Joo1234:Joo1234@cluster1.mydli6e.mongodb.net/?retryWrites=true&w=majority')
   .then(client => {
     console.log('connected!');
-    callback(client)
+    _db = client.db();
+    callback()
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err)
+    throw err;
+  });
+
 };
 
-module.exports = mongoConnect;
+const getDb =() => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found!'
+}
+
+exports.mongoConnect =mongoConnect;
+exports.getDb =getDb;
