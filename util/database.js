c36b72239753ab84d08c44a2b47d7747990eab5a
Yohannes <ydegu@admin.crossroads.co>
2023-02-15 09:58:16 +0300

    creating the db connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Joo1234', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongodb =require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://Joo1234:Joo1234@cluster1.mydli6e.mongodb.net/?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('connected!');
+    callback(client)
+  })
+  .catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
